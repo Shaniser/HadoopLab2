cd6c5497e8d510d0b562fd37b9b41266dafd085c
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 12:12:49 +0300

    Scripted auto-commit on change (2020-10-31 12:12:49) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportFlightsJoin.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportFlightsJoin.java
@@ -15,6 +15,7 @@ public class AirportFlightsJoin {
         job.setJarByClass(AirportFlightsJoin.class);
         job.setJobName("Airport join");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsListMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsListMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 
