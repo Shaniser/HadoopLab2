6f6ca75153afef90b86606901cf39ed68934b16f
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 11:26:47 +0300

    Scripted auto-commit on change (2020-10-31 11:26:47) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/FlightReducer.java
+++ src/main/java/ru/bmstu/hadoop/lab2/FlightReducer.java
@@ -3,7 +3,9 @@ package ru.bmstu.hadoop.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class FlightReducer extends Reducer<AirportWC, Text, Text, Text> {
     @Override
-    protected void reduce(AirportWC key, Iterable<Text> value, Context context)
+    protected void reduce(AirportWC key, Iterable<Text> value, Context context) throws IOException
 }
