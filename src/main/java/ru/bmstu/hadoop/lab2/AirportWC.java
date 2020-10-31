5b1dd48739a3cfce55a83e051c206d0204abd835
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 10:54:36 +0300

    Scripted auto-commit on change (2020-10-31 10:54:36) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportWC.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportWC.java
@@ -44,11 +44,11 @@ public class AirportWC implements WritableComparable {
     @Override
     public void write(DataOutput dataOutput) throws IOException {
         dataOutput.writeInt(id);
-        dataOutput.writeInt();
+        dataOutput.writeInt(indicator);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        
     }
 }
