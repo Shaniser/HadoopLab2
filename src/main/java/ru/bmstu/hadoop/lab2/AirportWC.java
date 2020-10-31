2856ce2d46756033466bd142d2dd3585b36d469e
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 10:26:59 +0300

    Scripted auto-commit on change (2020-10-31 10:26:58) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportWC.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportWC.java
@@ -1,2 +1,6 @@
-package ru.bmstu.hadoop.lab2;public class AirportWC {
+package ru.bmstu.hadoop.lab2;
+
+import org.apache.hadoop.io.WritableComparable;
+
+public class AirportWC implements WritableComparable {
 }
