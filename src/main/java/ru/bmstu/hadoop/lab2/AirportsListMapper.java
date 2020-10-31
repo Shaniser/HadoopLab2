230b1b2c229ae6e03fe0f03f7dee6d1415ece45a
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 11:49:44 +0300

    Scripted auto-commit on change (2020-10-31 11:49:44) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportsListMapper.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportsListMapper.java
@@ -4,7 +4,9 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportsListMapper extends Mapper<LongWritable, Text, AirportWC, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context)
+    protected void map(LongWritable key, Text value, Context context) throws IOException, 
 }
