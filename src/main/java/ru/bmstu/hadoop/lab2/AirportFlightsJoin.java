f073444ec50ba2afee3b1a768ca015d881948ac0
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 12:12:33 +0300

    Scripted auto-commit on change (2020-10-31 12:12:33) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportFlightsJoin.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportFlightsJoin.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AirportFlightsJoin {
@@ -12,7 +14,7 @@ public class AirportFlightsJoin {
         Job job = Job.getInstance();
         job.setJarByClass(AirportFlightsJoin.class);
         job.setJobName("Airport join");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsListMapper.class
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsListMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 
