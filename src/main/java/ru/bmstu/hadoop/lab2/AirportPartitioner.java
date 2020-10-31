d90ff58f2a2a51f29ee97af830d059b7589090b7
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 11:12:38 +0300

    Scripted auto-commit on change (2020-10-31 11:12:38) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportPartitioner.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportPartitioner.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<AirportWC, Text> {
     @Override
-    public int getPartition(AirportWC airportWC, Text text, int i) {
-        return 0;
+    public int getPartition(AirportWC airportWC, Text text, int numReduceTasks) {
+        return airportWC.getId() % numReduceTasks;
     }
 }
