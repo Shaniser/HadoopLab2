33c6c2fed72a38e682b8ee90ee6f27e41e4ec879
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 11:15:20 +0300

    Scripted auto-commit on change (2020-10-31 11:15:20) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/FlightGC.java
+++ src/main/java/ru/bmstu/hadoop/lab2/FlightGC.java
@@ -1,8 +1,9 @@
 package ru.bmstu.hadoop.lab2;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class FlightGC extends WritableComparator {
     @Override
-    public int compare()
+    public int compare(WritableComparable)
 }
