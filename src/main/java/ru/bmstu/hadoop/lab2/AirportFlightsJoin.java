cdf40933a4d4f3a9648bd8801e676de66541b907
Aleksey Kostyuchenko <mr.shaniser@gmail.com>
2020-10-31 11:02:50 +0300

    Scripted auto-commit on change (2020-10-31 11:02:50) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/lab2/AirportFlightsJoin.java
+++ src/main/java/ru/bmstu/hadoop/lab2/AirportFlightsJoin.java
@@ -1,4 +1,25 @@
 package ru.bmstu.hadoop.lab2;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class AirportFlightsJoin {
+    public static void main(String[] args) throws Exception {
+        Job job = Job.getInstance();
+        job.setJarByClass(AirportFlightsJoin.class);
+        job.setJobName("Airport join");
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
